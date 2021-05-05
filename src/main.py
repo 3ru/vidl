3f52d669cb9a84cd7c010969dc40017c350f7f92
Ryuyxx <Ryuyxx@gmail.com>
2021-05-05 16:20:56 +0900

    :broom: directory moved

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
-import os, re, requests, ffmpeg, shutil, youtube_dl, glob
+import re, requests, ffmpeg, youtube_dl, shutil, glob
+from src.settings import *
 import moviepy.editor as mp
-from settings import *
 
 
 def dl(vn):
     vl = []
-    os.makedirs("DL", exist_ok=True)
+    os.makedirs("../DL", exist_ok=True)
     for i in ["video", "audio"]:
-        ydl = youtube_dl.YoutubeDL({'outtmpl': f'tmp/%(id)s_{i}.%(ext)s', 'format': f'best{i}'})
+        ydl = youtube_dl.YoutubeDL({'outtmpl': f'../tmp/%(id)s_{i}.%(ext)s', 'format': f'best{i}'})
         ydl.extract_info(f'https://www.youtube.com/watch?v={vn}', download=True)
-        vl += glob.glob(f'tmp/{vn}_{i}*')
+        vl += glob.glob(f'../tmp/{vn}_{i}*')
 
     video = mp.VideoFileClip(vl[0])
     video = video.set_audio(mp.AudioFileClip(vl[1]))
-    video.write_videofile(f'DL/{vn}.mp4')
+    video.write_videofile(f'../DL/{vn}.mp4')
 
-    shutil.rmtree('tmp/')
+    # shutil.rmtree('./tmp/')
 
 
 def get_comment(video_id, n=30):
@@ -60,23 +60,22 @@ def splitter(vn, time_list, comment_list):
         elif len(t := i.split(':')) == 2:
             secs.append(int(t[0]) * 60 + int(t[1]))
 
-    stream = ffmpeg.input(f'DL/{vn}.mp4')
+    stream = ffmpeg.input(f'../DL/{vn}.mp4')
     for i, s in enumerate(secs):
-        trim = ffmpeg.output(stream, f'{vn}/{comment_list[i][:10]}.mp4', ss=s, t=secs[i + 1] - s)
-        thumb = ffmpeg.filter(stream, 'select', f'gte(n,{30 * (s + 1)})').output(f'{vn}/{comment_list[i][:10]}.jpg',
+        trim = ffmpeg.output(stream, f'../{vn}/{comment_list[i][:10]}.mp4', ss=s, t=secs[i + 1] - s)
+        thumb = ffmpeg.filter(stream, 'select', f'gte(n,{30 * (s + 1)})').output(f'../{vn}/{comment_list[i][:10]}.jpg',
                                                                                  vframes=1, format='image2',
                                                                                  vcodec='mjpeg')
         ffmpeg.run(trim)
         ffmpeg.run(thumb)
         if i == len(secs) - 2:
             break
-    os.rename(vn, "[済]-" + vn)
+    os.rename(f'../{vn}', f'../[済]-{vn}')
 
     return None
 
 
-# vid = "PyI1Y2vW1_Q"
-vid = "8IHWMapTnfw"
+vid = "DC6JppqHkaM"
 
 dl(vid)
 # times, comments = get_time_comment(get_comment(vid))
